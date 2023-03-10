95194dddaee5736b72204c15c28570f90b906b20
Pablo ieno <pabloieno@gmail.com>
2023-03-10 01:06:31 -0300

    Actualiza Ejercicio4

--- ejerciciosGuia2/src/main/java/egg/backend1/ejerciciosguia2/ejerc4.java
+++ ejerciciosGuia2/src/main/java/egg/backend1/ejerciciosguia2/ejerc4.java
@@ -14,7 +14,15 @@ public class ejerc4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* Define una variable que aloje tu nombre y otra que guarde tu edad. 
+           Imprime ambas variables por pantalla.*/
+        
+        String nombre = "Pablo";
+        int num1=49;
+        
+        System.out.println("Mi nombre es "+ nombre + " y tengo "+num1+ " años.");
+        
+
     }
     
 }
