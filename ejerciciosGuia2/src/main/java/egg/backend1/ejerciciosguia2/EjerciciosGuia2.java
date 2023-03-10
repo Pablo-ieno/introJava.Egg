deadde8ae8f3ffd9fe78671febae485dd0c40b9b
Pablo ieno <pabloieno@gmail.com>
2023-03-10 00:31:29 -0300

    Guia2_ejercicios

---
+++ ejerciciosGuia2/src/main/java/egg/backend1/ejerciciosguia2/EjerciciosGuia2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package egg.backend1.ejerciciosguia2;
+
+/**
+ *
+ * @author iz-living
+ */
+public class EjerciciosGuia2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
